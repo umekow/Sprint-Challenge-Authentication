ba51a1e1715dd6d92888ba841b3e3205eab07df8
umekow <umekolashun@gmail.com>
2019-11-15 10:32:52 -0600

    import libraries and create getJwtToken function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
-const Users
+
+const brcypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken'); 
+
+const Users = require('../users/users-model.js'); 
+
+//endpoints
 router.post('/register', (req, res) => {
   const {username, password} = req.body; 
 
@@ -10,4 +16,17 @@ router.post('/login', (req, res) => {
   // implement login
 });
 
+const getJwtToken = username => {
+  const payload = {
+    username
+  }; 
+
+  const secret = process.env.JWT_SECRET || "secret"; 
+  
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 module.exports = router;
